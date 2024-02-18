bb8cfc00111977c2f9e4b7912e1c79b2a40f41eb
Ryanwalker2 <148826569+Ryanwalker2@users.noreply.github.com>
2024-02-17 23:54:23 -0500

    Several update to functionality intermediate commit

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model:Category, model: ProductTag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No Products exist.'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model:Category }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product exists with this id.' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
