e8830f344a27c040c3634d710bdd3623e04a49cc
Ryanwalker2 <148826569+Ryanwalker2@users.noreply.github.com>
2024-02-18 23:21:11 -0500

    Generated README Edits to Product Post route - In Progress

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -47,6 +47,26 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+ try {
+  //if there are multiple products submitted, use bulkCreate, else use create.
+  if (req.body.length) {
+    Product.bulkCreate(req.body)
+    .then((product) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(product);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+  } else {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -63,10 +83,10 @@ router.post('/', (req, res) => {
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  }} catch (err) {
+    res.status(400).json(err);
+  }
+
 });
 
 // update product
